b168d491956ffbcaa65c8178278e03b14d53f2f3
tbednarz <tresbednarz@gmail.com>
2020-09-14 13:47:29 -0400

    Callback

--- read.js
+++ read.js
@@ -9,7 +9,7 @@ let credits = [];
 let debits = [];
 
 //read csv, seperate debits and credits, turn into objects and push to arrays.
-function formObjects(file) {
+function formObjects(file, callback) {
   fs.createReadStream(file)
     .pipe(csv())
     .on("data", (data) => results.push(data))
@@ -21,39 +21,44 @@ function formObjects(file) {
             Description: item["Description"],
             Amount: parseInt(item["Amount"]),
           };
-          return credits.push(obj);
+          credits.push(obj);
         }
         if (item["Credit or Debit"] === "Debit") {
           let obj = {
             Description: item["Description"],
             Amount: parseInt(item["Amount"]),
           };
-          return debits.push(obj);
+          debits.push(obj);
         }
       });
-      return credits;
+      callback(credits, debits);
     });
 }
-console.log(formObjects("august.csv"));
-// function calculateLoss() {
-//   console.log(formObjects(file));
-//   // let creditTotal = 0; // Variable to hold your total
 
-//   // for (let i = 0; i < credits.length; i++) {
-//   //   creditTotal += credits[i].Amount;
-//   // }
-
-//   // let debitTotal = 0;
-
-//   // for (let i = 0; i < debits.length; i++) {
-//   //   debitTotal += debits[i].Amount;
-//   // }
-//   // let savedOrLost = creditTotal - debitTotal;
-//   // if (savedOrLost > 0) {
-//   //   console.log("you saved: " + savedOrLost + " this month.");
-//   // }
-//   // if (savedOrLost < 0) {
-//   //   console.log("you lost: " + savedOrLost + " this month.");
-//   // }
-// }
-// calculateLoss();
+function callback(creditInput, debitInput) {
+  for (let i = 0; i < debits.length; i++) {
+    if (Object.values(debits[i]).includes("gopuff")) {
+      console.log("has test1");
+    } else {
+      console.log("Nope");
+    }
+  }
+  // let creditTotal = 0; // Variable to hold your total
+  // for (let i = 0; i < credits.length; i++) {
+  //   creditTotal += credits[i].Amount;
+  // }
+  // let debitTotal = 0;
+  // for (let i = 0; i < debits.length; i++) {
+  //   debitTotal += debits[i].Amount;
+  // }
+  // let savedOrLost = creditTotal - debitTotal;
+  // console.log("Credit total: " + creditTotal);
+  // console.log("Debit total: " + debitTotal);
+  // if (savedOrLost > 0) {
+  //   console.log("you saved: " + savedOrLost + " this month.");
+  // }
+  // if (savedOrLost < 0) {
+  //   console.log("you lost: " + savedOrLost + " this month.");
+  // }
+}
+formObjects("august.csv", callback);
